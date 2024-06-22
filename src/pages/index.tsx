2480956347cad0b986fcad531ef2690867901b03
inoue0124 <alice7124@gmail.com>
2024-06-23 02:05:22 +0900

    chore: Update homepage layout and description

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -49,10 +49,7 @@ function HomepageHeader() {
 export default function Home(): JSX.Element {
   const { siteConfig } = useDocusaurusContext();
   return (
-    <Layout
-      title="ホーム"
-      description="Description will go into a meta tag in <head />"
-    >
+    <Layout title="ホーム" description="マナテクのコース教材サイトです。">
       <HomepageHeader />
       <main>
         <HomepageFeatures />
